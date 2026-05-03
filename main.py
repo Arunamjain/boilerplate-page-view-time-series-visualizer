045756084cd2337df270cfb9f60217c773de3ee3
Arunamjain <166137622+Arunamjain@users.noreply.github.com>
2026-05-03 18:09:35 +0530

    Update main.py to run unit tests with unittest
    Refactor test execution to use unittest module directly.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-# This entrypoint file to be used in development. Start by reading README.md
 import time_series_visualizer
-from unittest import main
+import unittest
+import test_module
 
-# Test your function by calling it here
+unittest.main(module='test_module', exit=False, verbosity=2)
+
 time_series_visualizer.draw_line_plot()
 time_series_visualizer.draw_bar_plot()
 time_series_visualizer.draw_box_plot()
-
-# Run unit tests automatically
-main(module='test_module', exit=False)
